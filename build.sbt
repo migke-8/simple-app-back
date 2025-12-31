fb6e9db6bcdad76724600be58368e2e809cd8688
migke-8 <miguelportelabispo@gmail.com>
2025-12-31 13:41:09 -0300

    feat: mvp ready

--- build.sbt
+++ build.sbt
@@ -7,9 +7,9 @@ lazy val root = project
     version := "0.1.0-SNAPSHOT",
     scalaVersion := scala3Version,
     libraryDependencies += "org.scalameta" %% "munit" % "1.0.0" % Test,
-    libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.51.1.0",
+    libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.48.0.0",
     libraryDependencies += "org.scalikejdbc" %% "scalikejdbc" % "4.3.5",
     libraryDependencies += "com.lihaoyi" %% "upickle" % "4.4.1",
     libraryDependencies += "org.slf4j" % "slf4j-simple" % "2.0.16",
-    libraryDependencies += "io.javalin" % "javalin" % "6.7.0",
+    libraryDependencies += "io.javalin" % "javalin" % "6.7.0"
   )
--- src/main/scala/migke/app/Main.scala
+++ src/main/scala/migke/app/Main.scala
@@ -4,22 +4,22 @@ import scala.collection.mutable.ArrayBuffer
 import scalikejdbc.*
 
 @main def run(): Unit = {
-  DB.init()
+  BallsDB.init()
   val app = Javalin
     .create()
     .get(
       "/api/balls",
-      (ctx) => ctx.json(write(DB.all))
+      (ctx) => ctx.json(write(BallsDB.all))
     )
     .get(
       "/api/balls/{id}",
-      (ctx) => ctx.json(write(DB.find(ctx.pathParam("id").toLong)))
+      (ctx) => ctx.json(write(BallsDB.find(ctx.pathParam("id").toLong)))
     )
     .post(
       "/api/balls",
       (ctx) => {
         val received = read[BallDTO](ctx.body)
-        ctx.json(write(DB.add(received)))
+        ctx.json(write(BallsDB.add(received)))
       }
     )
     .start(8080)
@@ -28,20 +28,49 @@ import scalikejdbc.*
 case class Ball(id: Long, x: Int, y: Int, radius: Int) derives ReadWriter
 case class BallDTO(x: Int, y: Int, radius: Int) derives ReadWriter
 
-object DB {
-
-  given session: DBSession = AutoSession
+object BallsDB {
+  given dbSession: DBSession = NamedDB("db").autoCommitSession()
 
   def init() = {
     Class.forName("org.sqlite.JDBC")
-    ConnectionPool.singleton("jdbc:sqlite:database.db", "", "")
+    ConnectionPool.add("db", "jdbc:sqlite:database.db", null, null)
+    if (!BallsDB.tableExists("balls")) BallsDB.createTable()
   }
-  def all: List[Ball] = sql"select * from balls"
-    .map((rs) =>
-      Ball(rs.long("id"), rs.int("x"), rs.int("y"), rs.int("radius"))
-    )
+
+  def all: List[Ball] = sql"SELECT * FROM balls"
+    .map(extractor)
     .list
     .apply()
-  def find(id: Long): Ball = ???
-  def add(dto: BallDTO): Ball = ???
+  def find(id: Long): Option[Ball] = sql"""
+    SELECT * FROM balls where id = ${id}
+  """.map(extractor).single.apply()
+  def add(dto: BallDTO): Ball = Ball(
+    sql"""
+    INSERT INTO balls (x, y, radius) VALUES (${dto.x}, ${dto.y}, ${dto.radius})
+  """.updateAndReturnGeneratedKey.apply(),
+    dto.x,
+    dto.y,
+    dto.radius
+  )
+
+  def extractor(rs: WrappedResultSet) =
+    Ball(rs.long("id"), rs.int("x"), rs.int("y"), rs.int("radius"))
+
+  def createTable() =
+    sql"""create table balls (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      x  INTEGER NOT NULL,
+      y  INTEGER NOT NULL,
+      radius INTEGER NOT NULL CHECK (radius > 0)
+    )""".execute.apply()
+  def tableExists(tableName: String): Boolean =
+    sql"""
+    select name
+    from sqlite_master
+    where type = 'table' and name = $tableName
+  """
+      .map(_.string("name"))
+      .single
+      .apply()
+      .isDefined
 }
